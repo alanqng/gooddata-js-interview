e660119cce3095ac29f2db3853e3d6b2d13cac0b
z0041hhp <alan.ng@siemens.com>
2020-05-17 21:46:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,94 @@
 // Copyright (C) 2007-2019, GoodData(R) Corporation. All rights reserved.
 
-import React, { Component } from 'react';
+import React from 'react';
 import '@gooddata/react-components/styles/css/main.css';
 
-import { ColumnChart } from '@gooddata/react-components';
+import { ColumnChart, Model } from '@gooddata/react-components';
 
 const grossProfitMeasure = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/6877';
 const dateAttributeInMonths = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
 const dateAttribute = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2180';
 
-class App extends Component {
+const BarChartMonthly = ({measures, projectId, title, onChangeMonth, year, filters}) => {
+    const renderDropdown = () => {
+        return (
+            <select defaultValue="1" onChange={(event) => onChangeMonth(event)}>
+                <option value="1">January</option>
+                <option value="2">February</option>
+                <option value="3">March</option>
+                <option value="4">April</option>
+                <option value="5">May</option>
+                <option value="6">June</option>
+                <option value="7">July</option>
+                <option value="8">August</option>
+                <option value="9">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+            </select>
+        )
+    }
+    return (
+        <>
+            <h1>{title} {renderDropdown()} {year}</h1>
+            <div>
+                <ColumnChart
+                    measures={measures}
+                    filters={filters}
+                    projectId={projectId}
+                />
+            </div>
+        </>
+    )
+}
 
-    getMonthFilter() {
+const BarChartAll = ({measures, viewBy, projectId, title}) => {
+    return (
+        <>
+            <h1>{title}</h1>
+            <div>
+                <ColumnChart
+                measures={measures}
+                viewBy={viewBy}
+                projectId={projectId}
+                />
+            </div>
+            </>
+    )
+}
+
+class App extends React.Component {
+    state = {
+        month: 1
+    };
+
+    getMonthFilter = (month, year = 2016) => {
+        let lastDay = this.getDaysInMonth(new Date(2016, month)).toString()
         return {
             absoluteDateFilter: {
                 dataSet: {
                     uri: dateAttribute
                 },
-                from: '2016-01-01',
-                to: '2016-01-31'
+                from: `${year}-${(month)}-01`,
+                to: `${year}-${(month)}-${lastDay}`
             }
 
         }
     }
 
-    getMeasures() {
+    onChangeMonth = (event) => {
+        let monthValue = event.currentTarget.value
+        this.setState(prevState => {
+            return ({...prevState,
+                month: monthValue
+            })
+        })
+    }
+
+
+    getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+    getMeasures = () => {
         return [
             {
                 measure: {
@@ -42,64 +106,44 @@ class App extends Component {
         ]
     }
 
-    getViewBy() {
+    getViewBy = () => {
         return {
             visualizationAttribute:
-            {
-                displayForm: {
-                    uri: dateAttributeInMonths
-                },
-                localIdentifier: 'a1'
-            }
+                {
+                    displayForm: {
+                        uri: dateAttributeInMonths
+                    },
+                    localIdentifier: 'a1'
+                }
         }
     }
 
-    renderDropdown() {
-        return (
-            <select defaultValue="1">
-                <option value="1">January</option>
-                <option value="2">February</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-        )
-    }
-
     render() {
+
         const projectId = 'xms7ga4tf3g3nzucd8380o2bev8oeknp';
-        const filters = [this.getMonthFilter()];
+        const filters = [this.getMonthFilter(this.state.month)];
         const measures = this.getMeasures();
         const viewBy = this.getViewBy();
-
+        console.log(Model.attribute(grossProfitMeasure))
         return (
             <div className="App">
-                <h1>$ Gross Profit in month {this.renderDropdown()} 2016</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        filters={filters}
-                        projectId={projectId}
-                    />
-                </div>
-                <h1>$ Gross Profit - All months</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        viewBy={viewBy}
-                        projectId={projectId}
-                    />
-                </div>
+                <BarChartMonthly
+                    measures={measures}
+                    filters={filters}
+                    projectId={projectId}
+                    year={2016}
+                    title={'$ Gross Profit in month'}
+                    onChangeMonth={this.onChangeMonth}/>
+                <BarChartAll
+                    measures={measures}
+                    viewBy={viewBy}
+                    projectId={projectId}
+                    title={'$ Gross Profit - All months'}/>
             </div>
         );
     }
+
 }
 
 export default App;
+
